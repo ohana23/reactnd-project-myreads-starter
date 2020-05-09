e0542cd6753196da0c94bca4dfdaf121143878d2
Daniel Ohana <danny.ohana@gmail.com>
2020-05-08 22:44:38 -0400

    delete old comment

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-// import BooksApp from './components/BooksApp.js';
 import * as BooksAPI from './BooksAPI';
 import Search from './components/Search.js';
 import './App.css';
